077fa0c3f6a157d3b674e36937eaee901e04a550
Ildar Akhmetov <ildar@ualberta.ca>
2022-06-22 15:19:18 -0600

    Three tests

--- lab2/age1/__init__.py
+++ lab2/age1/__init__.py
@@ -9,7 +9,23 @@ def exists():
 
 @check50.check(exists)
 def test_samwise():
-    """input of 39 yields output of 4"""
+    """Samwise is younger than Frodo"""
     check50.run("python3 age1.py").stdin("Samwise").stdin("39").stdout(
         "Samwise is 39 years old, and they are younger than Frodo.\n"
+    ).exit()
+
+
+@check50.check(exists)
+def test_bilbo():
+    """Bilbo is older than Frodo"""
+    check50.run("python3 age1.py").stdin("Bilbo").stdin("129").stdout(
+        "Bilbo is 129 years old, and they are older than Frodo.\n"
+    ).exit()
+
+
+@check50.check(exists)
+def test_frodo():
+    """Frodo is of the same age as Frodo"""
+    check50.run("python3 age1.py").stdin("Frodo").stdin("51").stdout(
+        "Frodo is 51 years old, and they are of the same age as Frodo.\n"
     ).exit()
