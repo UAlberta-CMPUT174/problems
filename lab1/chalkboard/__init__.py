a166c7f06caa60ca07d5e5d65b23f41db8dbc54b
JoshuaBillson <jmbillson@outlook.com>
2022-07-05 15:38:50 -0600

    lab1/chalkboard.py

--- lab1/chalkboard/__init__.py
+++ lab1/chalkboard/__init__.py
@@ -10,10 +10,30 @@ def exists():
 
 
 @cs50.check(exists)
-def test_one():
+def test_garlic():
     """Test Passed For Negative Input"""
     cs50.run(f"python3 {SCRIPT_NAME}") \
         .stdin("Garlic gum is not funny.") \
         .stdin("3") \
         .stdout("Garlic gum is not funny. Garlic gum is not funny. Garlic gum is not funny.") \
+        .exit()
+
+
+@cs50.check(exists)
+def test_paint():
+    """Test Passed For Negative Input"""
+    cs50.run(f"python3 {SCRIPT_NAME}") \
+        .stdin("I will not drive the principal's car.") \
+        .stdin("1") \
+        .stdout("I will not drive the principal's car.") \
+        .exit()
+
+
+@cs50.check(exists)
+def test_attitude():
+    """Test Passed For Negative Input"""
+    cs50.run(f"python3 {SCRIPT_NAME}") \
+        .stdin("I will not get very far with this attitude.") \
+        .stdin("4") \
+        .stdout("I will not get very far with this attitude. I will not get very far with this attitude. I will not get very far with this attitude. I will not get very far with this attitude.") \
         .exit()
