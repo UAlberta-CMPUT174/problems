a2defafc7f1002bae8b5ddff1842ce12afd71f73
JoshuaBillson <jmbillson@outlook.com>
2022-07-05 14:32:10 -0600

    lab1/temperature.py

--- lab1/temperature/__init__.py
+++ lab1/temperature/__init__.py
@@ -1,16 +1,18 @@
 import check50 as cs50
 
 
+SCRIPT_NAME = "temperature.py"
+
 @cs50.check()
 def exists():
-    """temperature.py exists"""
-    cs50.exists("temperature.py")
+    cs50.exists(SCRIPT_NAME)
+exists.__doc__ = f"{SCRIPT_NAME} exists"
 
 
 @cs50.check(exists)
 def test_negative():
     """Test Passed For Negative Input"""
-    cs50.run("python3 temperature.py") \
+    cs50.run(f"python3 {SCRIPT_NAME}") \
         .stdin("-30") \
         .stdout("-30 degrees in Canada would be -22 degrees in Springfield. D'oh!") \
         .exit()
@@ -19,7 +21,7 @@ def test_negative():
 @cs50.check(exists)
 def test_positive():
     """Test Passed For Positive Input"""
-    cs50.run("python3 temperature.py") \
+    cs50.run(f"python3 {SCRIPT_NAME}") \
         .stdin("30") \
         .stdout("30 degrees in Canada would be 86 degrees in Springfield. D'oh!") \
         .exit()
@@ -28,7 +30,7 @@ def test_positive():
 @cs50.check(exists)
 def test_zero():
     """Test Passed For Zero Input"""
-    cs50.run("python3 temperature.py") \
+    cs50.run(f"python3 {SCRIPT_NAME}") \
         .stdin("0") \
         .stdout("0 degrees in Canada would be 32 degrees in Springfield. D'oh!") \
         .exit()
