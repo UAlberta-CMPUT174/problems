357d4f7224b94da50fa8606b5a6f738e2c8ef2d3
JoshuaBillson <jmbillson@outlook.com>
2022-07-20 22:41:15 -0600

    Added Tests for age2, age3, age4

---
+++ lab2/age2/__init__.py
@@ -0,0 +1,39 @@
+import check50 as cs50
+
+
+SCRIPT_NAME = "age2.py"
+
+@cs50.check()
+def exists():
+    """age1.py exists"""
+    cs50.exists(SCRIPT_NAME)
+
+
+@cs50.check(exists)
+def test_samwise():
+    """Samwise is younger than Frodo"""
+    cs50.run(f"python3 {SCRIPT_NAME}") \
+        .stdin("Samwise") \
+        .stdin("39") \
+        .stdout("Samwise is 39 years old, and they are younger than Frodo and Gollum.") \
+        .exit()
+
+
+@cs50.check(exists)
+def test_bilbo():
+    """Bilbo is older than Frodo"""
+    cs50.run("python3 age1.py") \
+        .stdin("Bilbo") \
+        .stdin("129") \
+        .stdout("Bilbo is 129 years old, and they are older than Frodo.\n") \
+        .exit()
+
+
+@cs50.check(exists)
+def test_frodo():
+    """Frodo is of the same age as Frodo"""
+    cs50.run("python3 age1.py") \
+        .stdin("Frodo") \
+        .stdin("51") \
+        .stdout("Frodo is 51 years old, and they are of the same age as Frodo.\n") \
+        .exit()
