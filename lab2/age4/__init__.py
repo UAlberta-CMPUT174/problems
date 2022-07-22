49cacb8fbadcd9e52fcad828b480e6f1e1d2b1b0
JoshuaBillson <jmbillson@outlook.com>
2022-07-22 00:32:44 -0600

    Implemented tests for age4

--- lab2/age4/__init__.py
+++ lab2/age4/__init__.py
@@ -1,39 +1,49 @@
 import check50 as cs50
 
 
-SCRIPT_NAME = "age1.py"
+SCRIPT_NAME = "age4.py"
 
 @cs50.check()
 def exists():
-    """age1.py exists"""
+    """age4.py exists"""
     cs50.exists(SCRIPT_NAME)
 
 
 @cs50.check(exists)
-def test_samwise():
-    """Samwise is younger than Frodo"""
+def test_eowyn():
+    """Eowyn is younger than Frodo, Samwise, Gandalf, Legolas, Gimli, Aragorn, Boromir, Merry, Pippin."""
     cs50.run(f"python3 {SCRIPT_NAME}") \
-        .stdin("Samwise") \
-        .stdin("39") \
-        .stdout("Samwise is 39 years old, and they are younger than Frodo.\n") \
+        .stdin("Eowyn") \
+        .stdin("24") \
+        .stdout("Eowyn is 24 years old, and they are younger than Frodo, Samwise, Gandalf, Legolas, Gimli, Aragorn, Boromir, Merry, Pippin.") \
         .exit()
 
 
 @cs50.check(exists)
 def test_bilbo():
-    """Bilbo is older than Frodo"""
-    cs50.run("python3 age1.py") \
+    """Bilbo is older than Frodo, Samwise, Aragorn, Boromir, Merry, Pippin and younger than Gandalf, Legolas, Gimli."""
+    cs50.run(f"python3 {SCRIPT_NAME}") \
         .stdin("Bilbo") \
         .stdin("129") \
-        .stdout("Bilbo is 129 years old, and they are older than Frodo.\n") \
+        .stdout("Bilbo is 129 years old, and they are older than Frodo, Samwise, Aragorn, Boromir, Merry, Pippin.\nBilbo is 129 years old, and they are younger than Gandalf, Legolas, Gimli.") \
         .exit()
 
 
 @cs50.check(exists)
-def test_frodo():
-    """Frodo is of the same age as Frodo"""
-    cs50.run("python3 age1.py") \
-        .stdin("Frodo") \
-        .stdin("51") \
-        .stdout("Frodo is 51 years old, and they are of the same age as Frodo.\n") \
+def test_galadriel():
+    """Galadriel is older than Frodo, Samwise, Gandalf, Legolas, Gimli, Aragorn, Boromir, Merry, Pippin."""
+    cs50.run(f"python3 {SCRIPT_NAME}") \
+        .stdin("Galadriel") \
+        .stdin("7000") \
+        .stdout("Galadriel is 7000 years old, and they are older than Frodo, Samwise, Gandalf, Legolas, Gimli, Aragorn, Boromir, Merry, Pippin.") \
+        .exit()
+
+
+@cs50.check(exists)
+def test_invalid():
+    """Ages Cannot Be Negative"""
+    cs50.run(f"python3 {SCRIPT_NAME}") \
+        .stdin("Foo") \
+        .stdin("-1") \
+        .stdout("Invalid age.") \
         .exit()
