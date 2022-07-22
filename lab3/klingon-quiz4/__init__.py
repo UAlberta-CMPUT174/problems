eb74c36453eab8c19afacf5b4f13822c39c31fba
JoshuaBillson <jmbillson@outlook.com>
2022-07-22 11:57:10 -0600

    Added skeleton code for klingon-quiz2, klingon-quiz3, klingon-quiz 4

---
+++ lab3/klingon-quiz4/__init__.py
@@ -0,0 +1,18 @@
+import check50 as cs50
+
+
+SCRIPT_NAME = "klingon-quiz1.py"
+
+@cs50.check()
+def exists():
+    """klingon-quiz1.py exists"""
+    cs50.exists(SCRIPT_NAME)
+
+
+@cs50.check(exists)
+def test_correct():
+    """Correct, Computer is De'wI' in Klingon"""
+    cs50.run(f"python3 {SCRIPT_NAME}") \
+        .stdin("De'wI'") \
+        .stdout("Correct!") \
+        .exit()
