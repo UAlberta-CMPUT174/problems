20a3fa20b64ae0af5e531ec855cd5180e53e406e
JoshuaBillson <jmbillson@outlook.com>
2022-08-08 11:24:37 -0600

    Added Test To lab3/klingon-quiz3.py

--- lab3/klingon-quiz3/__init__.py
+++ lab3/klingon-quiz3/__init__.py
@@ -16,7 +16,8 @@ def noun_incorrect_3():
     """Incorrect: The Answer Is batlh"""
     attempt_1 = cs50.run(f"python3 {SCRIPT_NAME}").stdin("n").stdin("gh").stdin("ghighi").stdout()
 
-    if "How do you translate necklace to Klingon? You have 2 attempts left.\nHint: g___h\n>" == attempt_1.rstrip():
+    raise cs50.Mismatch("How do you translate necklace to Klingon? You have 2 attempts left.\nHint: g___h\n>", attempt_1.rstrip())
+    if "How do you translate necklace to Klingon? You have 2 attempts left.\nHint: g___h\n>" != attempt_1.rstrip():
         raise cs50.Mismatch("How do you translate necklace to Klingon? You have 2 attempts left.\nHint: g___h\n>", attempt_1.rstrip())
 
     attempt_2 = attempt_1.stdin("ghighi").stdout()
