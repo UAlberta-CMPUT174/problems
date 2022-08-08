b2960fdeaba6426c529e709c7883c1729b40a7ac
JoshuaBillson <jmbillson@outlook.com>
2022-08-08 14:40:19 -0600

    Added Test To lab4/gravity2.py

--- lab4/gravity2/__init__.py
+++ lab4/gravity2/__init__.py
@@ -11,8 +11,17 @@ def exists():
 
 @cs50.check(exists)
 def test_uppercase_and_space():
-    """Correct: The Secret Message Is: 'STAN IS NOT WHAT HE SEEMS.'"""
+    """Correct: The Sectret Message Is 'SORRY, DIPPER, BUT YOUR WENDY IS IN ANOTHER CASTLE.'"""
     output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("HLIIB, WRKKVI, YFG BLFI DVMWB RH RM ZMLGSVI XZHGOV.").stdout()
     correct_output = "Let's try all the methods we have:\nCaesar cipher: EIFFY, TOHHSF, VCD YICF ASJTY OE OJ WJIDPSF UWEDLS.\nAtbash cipher: SORRY, DIPPER, BUT YOUR WENDY IS IN ANOTHER CASTLE."
+    if correct_output != output.rstrip():
+        raise cs50.Mismatch(correct_output, output.rstrip())
+
+
+@cs50.check(exists)
+def test_lowercase_and_symbols():
+    """Correct: The Sectret Message Is 'Python is fun! :-)'"""
+    output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("Sbwkrq lv ixq! :-)").stdout()
+    correct_output = "Let's try all the methods we have:\nCaesar cipher: Python is fun! :-)\nAtbash cipher: Hydpij oe rcj! :-)"
     if correct_output != output.rstrip():
         raise cs50.Mismatch(correct_output, output.rstrip())
