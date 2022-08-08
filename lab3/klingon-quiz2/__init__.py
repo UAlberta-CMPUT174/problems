dbbb8fbc6cf3a26b3062f0216c492fd26e0c6fb7
JoshuaBillson <jmbillson@outlook.com>
2022-08-08 10:48:55 -0600

    Added Test For lab3/klingon-quiz2.py

--- lab3/klingon-quiz2/__init__.py
+++ lab3/klingon-quiz2/__init__.py
@@ -13,13 +13,14 @@ def exists():
 
 @cs50.check(exists)
 def test_noun_incorrect():
-    """The Correct Answer Is batlh"""
+    """Incorrect: The Answer Is batlh"""
     cs50.run(f"python3 {SCRIPT_NAME}") \
         .stdin("n") \
         .stdin("b") \
         .stdin("bal") \
         .stdout("Sorry, the correct answer is batlh.") \
         .exit()
+
 
 @cs50.check(exists)
 def test_noun_correct():
@@ -29,4 +30,37 @@ def test_noun_correct():
         .stdin("gh") \
         .stdin("ghIgh") \
         .stdout("Correct!") \
+        .exit()
+
+
+@cs50.check(exists)
+def test_verb_incorrect():
+    """Incorrect: The Answer Is Qochbe"""
+    cs50.run(f"python3 {SCRIPT_NAME}") \
+        .stdin("v") \
+        .stdin("m") \
+        .stdin("mevil") \
+        .stdout("Sorry, the correct answer is mIgh.") \
+        .exit()
+
+
+@cs50.check(exists)
+def test_verb_correct():
+    """Correct: The Answer Is Qochbe'!"""
+    cs50.run(f"python3 {SCRIPT_NAME}") \
+        .stdin("v") \
+        .stdin("Q") \
+        .stdin("Qochbe'") \
+        .stdout("Correct!") \
+        .exit()
+
+
+@cs50.check(exists)
+def test_capitalization():
+    """Incorrect: The Answer Is Qochbe', Not qochbe'"""
+    cs50.run(f"python3 {SCRIPT_NAME}") \
+        .stdin("v") \
+        .stdin("Q") \
+        .stdin("qochbe'") \
+        .stdout("Sorry, the correct answer is Qochbe'.") \
         .exit()
