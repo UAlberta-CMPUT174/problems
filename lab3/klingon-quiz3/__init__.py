6e5cc9dafa7810f494f61dea94c8b6f03915e3cf
JoshuaBillson <jmbillson@outlook.com>
2022-08-08 12:27:03 -0600

    Added Test To lab3/klingon-quiz3.py

--- lab3/klingon-quiz3/__init__.py
+++ lab3/klingon-quiz3/__init__.py
@@ -40,7 +40,7 @@ def noun_correct():
     """The Script Accepts A Correct Noun On The First Try"""
     output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("n").stdin("gh").stdin("ghIgh").stdout()
     if "Correct!\nThe correct answer was ghIgh." != output.rstrip():
-        raise cs50.Mismatch("Correct!", output.rstrip())
+        raise cs50.Mismatch("Correct!\nThe correct answer was ghIgh.", output.rstrip())
 
 
 @cs50.check(exists)
@@ -49,6 +49,30 @@ def verb_incorrect_3():
     output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("v").stdin("Q").stdin("quche").stdin("quche").stdin("quche").stdout()
     if "Sorry, you're wrong!\nThe correct answer was Qochbe'." != output.rstrip():
         raise cs50.Mismatch("Sorry, you're wrong!\nThe correct answer was ghIgh.", output.rstrip())
+
+
+@cs50.check(exists)
+def verb_incorrect_2():
+    """The Script Terminates And Reveals The Correct Verb After 3 Incorrect Verbs"""
+    output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("v").stdin("Q").stdin("quche").stdin("quche").stdin("Qochbe'").stdout()
+    if "Correct!" != output.rstrip():
+        raise cs50.Mismatch("Correct!", output.rstrip())
+
+
+@cs50.check(exists)
+def verb_incorrect_1():
+    """The Script Accepts A Correct Verb After 2 Incorrect Verbs"""
+    output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("v").stdin("Q").stdin("quche").stdin("Qochbe'").stdout()
+    if "Correct!" != output.rstrip():
+        raise cs50.Mismatch("Correct!", output.rstrip())
+
+
+@cs50.check(exists)
+def verb_correct():
+    """The Script Accepts A Correct Verb After 1 Incorrect Verbs"""
+    output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("v").stdin("Q").stdin("Qochbe'").stdout()
+    if "Correct!\nThe correct answer was Qochbe'." != output.rstrip():
+        raise cs50.Mismatch("Correct!\nThe correct answer was Qochbe'.", output.rstrip())
 
 
 @cs50.check(exists)
