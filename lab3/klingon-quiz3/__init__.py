6b44b0cf90e15dcb8d3963db83db0706441d7a6a
JoshuaBillson <jmbillson@outlook.com>
2022-08-08 12:04:06 -0600

    Added Test To lab3/klingon-quiz3.py

--- lab3/klingon-quiz3/__init__.py
+++ lab3/klingon-quiz3/__init__.py
@@ -17,4 +17,5 @@ Hint: g___h
 @cs50.check(exists)
 def noun_incorrect_3():
     """Incorrect: The Answer Is batlh"""
-    output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("n").stdin("gh").stdin("ghighi").stdout(x).stdin("ghighi").stdout("asdasd").stdin("ghighi").stdout("asdasd").exit()
+    output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("n").stdin("gh").stdin("ghighi").stdin("ghighi").stdin("ghighi").stdout()
+    print(output)
