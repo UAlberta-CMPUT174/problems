ed0e0c5fbfc34b99f9cef5dd533115f286795b92
JoshuaBillson <jmbillson@outlook.com>
2022-08-08 12:31:23 -0600

    Added Skeleton Code For Lab3/klingon-quiz4.py

--- lab3/klingon-quiz4/__init__.py
+++ lab3/klingon-quiz4/__init__.py
@@ -10,9 +10,72 @@ def exists():
 
 
 @cs50.check(exists)
-def test_correct():
-    """Correct, Computer is De'wI' in Klingon"""
-    cs50.run(f"python3 {SCRIPT_NAME}") \
-        .stdin("De'wI'") \
-        .stdout("Correct!") \
-        .exit()
+def noun_incorrect_3():
+    """The Script Terminates And Reveals The Correct Noun After 3 Incorrect Nouns"""
+    output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("n").stdin("gh").stdin("ghighi").stdin("ghighi").stdin("ghighi").stdout()
+    if "Sorry, you're wrong!\nThe correct answer was ghIgh." != output.rstrip():
+        raise cs50.Mismatch("Sorry, you're wrong!\nThe correct answer was ghIgh.", output.rstrip())
+    
+
+@cs50.check(exists)
+def noun_incorrect_2():
+    """The Script Accepts A Correct Noun After 2 Incorrect Nouns"""
+    output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("n").stdin("gh").stdin("ghighi").stdin("ghighi").stdin("ghIgh").stdout()
+    if "Correct!" != output.rstrip():
+        raise cs50.Mismatch("Correct!", output.rstrip())
+    
+
+@cs50.check(exists)
+def noun_incorrect_1():
+    """The Script Accepts A Correct Noun After 1 Incorrect Nouns"""
+    output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("n").stdin("gh").stdin("ghighi").stdin("ghIgh").stdout()
+    if "Correct!" != output.rstrip():
+        raise cs50.Mismatch("Correct!", output.rstrip())
+    
+
+@cs50.check(exists)
+def noun_correct():
+    """The Script Accepts A Correct Noun On The First Try"""
+    output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("n").stdin("gh").stdin("ghIgh").stdout()
+    if "Correct!\nThe correct answer was ghIgh." != output.rstrip():
+        raise cs50.Mismatch("Correct!\nThe correct answer was ghIgh.", output.rstrip())
+
+
+@cs50.check(exists)
+def verb_incorrect_3():
+    """The Script Terminates And Reveals The Correct Verb After 3 Incorrect Verbs"""
+    output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("v").stdin("Q").stdin("quche").stdin("quche").stdin("quche").stdout()
+    if "Sorry, you're wrong!\nThe correct answer was Qochbe'." != output.rstrip():
+        raise cs50.Mismatch("Sorry, you're wrong!\nThe correct answer was ghIgh.", output.rstrip())
+
+
+@cs50.check(exists)
+def verb_incorrect_2():
+    """The Script Terminates And Reveals The Correct Verb After 3 Incorrect Verbs"""
+    output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("v").stdin("Q").stdin("quche").stdin("quche").stdin("Qochbe'").stdout()
+    if "Correct!" != output.rstrip():
+        raise cs50.Mismatch("Correct!", output.rstrip())
+
+
+@cs50.check(exists)
+def verb_incorrect_1():
+    """The Script Accepts A Correct Verb After 2 Incorrect Verbs"""
+    output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("v").stdin("Q").stdin("quche").stdin("Qochbe'").stdout()
+    if "Correct!" != output.rstrip():
+        raise cs50.Mismatch("Correct!", output.rstrip())
+
+
+@cs50.check(exists)
+def verb_correct():
+    """The Script Accepts A Correct Verb After 1 Incorrect Verbs"""
+    output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("v").stdin("Q").stdin("Qochbe'").stdout()
+    if "Correct!\nThe correct answer was Qochbe'." != output.rstrip():
+        raise cs50.Mismatch("Correct!\nThe correct answer was Qochbe'.", output.rstrip())
+
+
+@cs50.check(exists)
+def case_sensitive():
+    """The Script Correctly Detects Letter Case"""
+    output = cs50.run(f"python3 {SCRIPT_NAME}").stdin("n").stdin("gh").stdin("ghigh").stdin("ghigh").stdin("ghigh").stdout()
+    if "Sorry, you're wrong!\nThe correct answer was ghIgh." != output.rstrip():
+        raise cs50.Mismatch("Sorry, you're wrong!\nThe correct answer was ghIgh.", output.rstrip())
