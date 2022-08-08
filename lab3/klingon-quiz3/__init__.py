2cbdf12e264db1eedbf8ab0396d59db714e74c4c
JoshuaBillson <jmbillson@outlook.com>
2022-08-08 11:04:34 -0600

    Added Test To lab3/klingon-quiz3.py

--- lab3/klingon-quiz3/__init__.py
+++ lab3/klingon-quiz3/__init__.py
@@ -3,6 +3,8 @@ import check50 as cs50
 
 SCRIPT_NAME = "klingon-quiz3.py"
 
+"b, ch, D, gh, H, j, l, m, n, p, q, Q, r, S, t, v, w, y,"
+
 @cs50.check()
 def exists():
     """klingon-quiz3.py exists"""
@@ -10,9 +12,15 @@ def exists():
 
 
 @cs50.check(exists)
-def test_correct():
-    """Correct, Computer is De'wI' in Klingon"""
+def noun_incorrect_3():
+    """Incorrect: The Answer Is batlh"""
     cs50.run(f"python3 {SCRIPT_NAME}") \
-        .stdin("De'wI'") \
-        .stdout("Correct!") \
+        .stdin("n") \
+        .stdin("gh") \
+        .stdin("ghighi") \
+        .stdout("How do you translate necklace to Klingon? You have 2 attempts left.\nHint: g___h") \
+        .stdin("ghighi") \
+        .stdout("Sorry, you're wrong!\nHow do you translate necklace to Klingon?\nYou have 1 attempts left.\nHint: g___h") \
+        .stdin("ghighi") \
+        .stdout("Sorry, you're wrong!\nThe correct answer was ghIgh.") \
         .exit()
